15dc1629460d28209d247db02da2d584738131f5
rrawwwrrr <39938067+rrawwwrrr@users.noreply.github.com>
2025-06-27 17:12:02 +0400

    Update goios.sh

--- goios.sh
+++ goios.sh
@@ -1,7 +1,7 @@
 sysctl -w net.ipv6.conf.all.disable_ipv6=0
 sysctl -w net.ipv6.conf.default.disable_ipv6=0
 mkdir -p /var/logs/
-export ENABLE_GO_IOS_AGENT=kernel
+export ENABLE_GO_IOS_AGENT=user
 usbmuxd&
 echo "Грузим образ xcode"
 /goios image auto >> /var/logs/image 2>&1
