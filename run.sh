dd4f6f9965c5cbcc9762ffc8b7dbb0b3269ada35
rrawwwrrr <39938067+rrawwwrrr@users.noreply.github.com>
2025-06-27 17:13:51 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ export USBMUXD_SOCKET_ADDRESS=/var/run/usbmuxd
 socat -d -d -d TCP4-LISTEN:27015,reuseaddr,fork UNIX-CONNECT:${USBMUXD_SOCKET_ADDRESS}&
 
 usbmuxd &
-./run.sh
+./goios.sh
 #sleep 5
 ##idevicepair pair
 #ideviceinfo
