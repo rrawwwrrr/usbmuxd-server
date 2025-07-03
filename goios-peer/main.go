79b9f0bd006c494a2b517490f1390e4b600c593d
rrr <ruslan.ramazanov@antara-tech.ru>
2025-07-03 21:54:49 +0400

    init

--- goios-peer/main.go
+++ goios-peer/main.go
@@ -7,6 +7,12 @@ import (
 )
 
 func main() {
+	customFormatter := new(log.TextFormatter)
+	customFormatter.TimestampFormat = "2006-01-02 15:04:05"
+	log.SetFormatter(customFormatter)
+	log.Info("Hello Walrus before FullTimestamp=true")
+	customFormatter.FullTimestamp = true
+	log.Info("Hello Walrus after FullTimestamp=true")
 	log.SetLevel(log.TraceLevel)
 	goios.Start()
 }
