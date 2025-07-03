010dc63510224ae32065c2fa6c4a24a80b7476d2
rrr <ruslan.ramazanov@antara-tech.ru>
2025-07-03 20:13:59 +0400

    init

--- goios-peer/main.go
+++ goios-peer/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"goios-peer/goios"
-	//log "github.com/sirupsen/logrus"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	//log.SetLevel(log.TraceLevel)
+	log.SetLevel(log.TraceLevel)
 	goios.Start()
 }
